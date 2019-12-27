9a18ea3fab64a022d67bccd0364c418a3727a3ca
kdord <katya.koliada49@gmail.com>
2019-12-27 16:55:26 +0200

    add Meal component with routeProps

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import './App.css';
 import Food from './Food';
+import Meal from './Meal';
 
-function App() {
-  return (
-    <div className='App'>
-      <Route exaxt path='/food/:name' component={Food} />
-      {/* <Food name='Kale' />
-      <Food name='Chicken' />
-      <Food name='Banana' /> */}
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Route
+          exact
+          path='/food/:name'
+          render={routeProps => <Food {...routeProps} />}
+        />
+
+        <Route exact path='/food/:foodName/drink/:drinkName' component={Meal} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Meal.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Meal extends Component {
+  render() {
+    const foodName = this.props.match.params.foodName;
+    const drinkName = this.props.match.params.drinkName;
+    const foodUrl = `https://source.unsplash.com/1600x900/?${foodName}`;
+    const drinkUrl = `https://source.unsplash.com/1600x900/?${drinkName}`;
+
+    return (
+      <div>
+        <h1>
+          This is a meal {foodName} + {drinkName}
+        </h1>
+        <img src={foodUrl} alt={foodUrl} />
+        <img src={drinkUrl} alt={drinkUrl} />
+      </div>
+    );
+  }
+}
+
+export default Meal;
